0e10edfdb00c762776ffa64958e4f3dacb927e3a
ilnar iskhakov <ilnar150595@gmail.com>
2022-12-11 19:10:00 +0300

    commit10

--- project/video_python.py
+++ project/video_python.py
@@ -4,8 +4,8 @@ import time
 
 
 #
-def zoom_frames(src, dim, k=1):  # получить картинку для отдаленного разрешения
-    dim_max = (int(dim[0] * k), int(dim[1] * k))  # нужно ввести данные для отдаленного разрешения
+def zoom_frames(src, dim, zoom):  # получить картинку для отдаленного разрешения
+    dim_max = (int(dim[0] * zoom), int(dim[1] * zoom))  # нужно ввести данные для отдаленного разрешения
     width = dim_max[0]
     height = dim_max[1]
     if src.shape[1] < dim_max[0]:
@@ -22,7 +22,7 @@ def zoom_frames(src, dim, k=1):  # получить картинку для от
     return output
 
 
-def zoom_in_zoom_out(frame, effectName, dim, frame_time, fps, i, zoom=1):
+def zoom_in_zoom_out(frame, effectName, dim, frame_time, fps, i, zoom):
     centre_width = frame.shape[1] / 2
     centre_height = frame.shape[0] / 2
     withdif = centre_width - (dim[0] / 2)
@@ -47,7 +47,7 @@ def zoom_in_zoom_out(frame, effectName, dim, frame_time, fps, i, zoom=1):
     return cv2.resize(frame[y1:y2, x1:x2], dim, interpolation=cv2.INTER_LINEAR)
 
 
-def move_zoom_frame(frame, effectName, dim, frame_time, fps, i, zoom=1):
+def move_zoom_frame(frame, effectName, dim, frame_time, fps, i, zoom):
     k = i / frame_time * 1000 / fps
     withdif = frame.shape[1] - dim[0]
     heightdif = frame.shape[0] - dim[1]
@@ -78,22 +78,37 @@ def move_zoom_frame(frame, effectName, dim, frame_time, fps, i, zoom=1):
     # print("(x1", x1, "y1", y1, ")(x2", x2, "y2", y2, "                    k", k)
     return cv2.resize(frame[y1:y2, x1:x2], dim, interpolation=cv2.INTER_LINEAR)
 
+def zoom_without_effects(frame, dim, frame_time, fps, i, zoom):
+    centre_width = frame.shape[1] / 2
+    centre_height = frame.shape[0] / 2
+    withdif = centre_width - (dim[0] / 2)
+    heightdif = centre_height - (dim[1] / 2)
+    if frame.shape[1] / dim[0] > frame.shape[0] / dim[1]:
+        a = heightdif * dim[0] / dim[1]
+        b = heightdif
+    else:
+        a = withdif
+        b = withdif * dim[1] / dim[0]
+    c = 1
+    x1 = int(centre_width - (zoom * dim[0] / 2) + a * c)
+    x2 = int(centre_width + (zoom * dim[0] / 2) - a * c)
+    y1 = int(centre_height - (zoom * dim[1] / 2) + b * c)
+    y2 = int(centre_height + (zoom * dim[1] / 2) - b * c)
+    # print("(x1", x1, "y1", y1, ")(x2", x2, "y2", y2, "                    k", k)
+    return cv2.resize(frame[y1:y2, x1:x2], dim, interpolation=cv2.INTER_LINEAR)
+
 
 # Define the codec and create VideoWriter object
 fps = 30
-dim = (1366, 720)
-k1 = {'10': 1.20, '20': 1.2, '30': 1.30, '40': 1.4, '50': 1.5, '60': 1.6, '70': 1.7, '80': 1.8, '90': 1.9, '100': 2.0}
-zoom = k1.get('100')
-frame_time = 3000
-effectName = "zoomIn"
-# effectName = (
-#     ("zoomIn", "zoomIn"),
-#     ("zoomOut", "zoomOut"),
-#     ("moveTop", " moveTop"),
-#     ("moveLeft", " moveLeft"),
-#     ("moveRight", "moveRight"),
-#     ("moveBottom", "moveBottom"))
-# fourcc = cv.VideoWriter_fourcc(*"mp4v")
+dim = (1366, 768)
+k1 = {'10%': 1.20, '20%': 1.2, '30%': 1.30, '40%': 1.4, '50%': 1.5, '60%': 1.6, '70%': 1.7, '80%': 1.8, '90%': 1.9, '100%': 2.0}
+zoom = k1.get('50%', 1)
+frame_time = 3000#3000
+effectName = "zoomIn"       # zoomIn        zoomOut     moveTop  moveLeft  moveRight  moveBottom
+if not zoom:
+    zoom = 1
+if not frame_time:
+    frame_time = 1000
 out = cv2.VideoWriter('output1.avi', cv2.VideoWriter_fourcc(*"mp4v"), fps, dim)
 
 quantity_frame: int = int(fps * frame_time // 1000)
@@ -107,13 +122,16 @@ if effectName == "zoomIn" or effectName == "zoomOut":
         resized = zoom_in_zoom_out(frame, effectName, dim, frame_time, fps, i,
                                    zoom)  # обрезка кадра для i-го изображения
         out.write(resized)
-elif effectName == "moveTop" or effectName == "moveLeft" or effectName == "moveRight" or effectName == "moveBottom":
+elif effectName == "moveTop" or effectName == "moveLeft" or effectName == "moveRight":
     for i in range(quantity_frame):
         resized = move_zoom_frame(frame, effectName, dim, frame_time, fps, i,
                                   zoom)  # обрезка кадра для i-го изображения
         out.write(resized)
 else:
-    pass
+    for i in range(quantity_frame):
+        resized = zoom_without_effects(frame, dim, frame_time, fps, i,
+                                   zoom)  # обрезка кадра для i-го изображения
+        out.write(resized)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 # Release everything if job is finished
